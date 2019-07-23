b394c4692827ecaf7d3500861c0323113286d6a8
gabrielott <gabrielott@icloud.com>
2019-07-23 10:35:02 -0300

    Limpeza das seleções anteriores no menu opções agora é mais robusta

--- src/menus.c
+++ src/menus.c
@@ -320,7 +320,10 @@ void optionsmenu(void) {
 
 			char formatted[20];
 			strcpy(formatted, options[i]->options[options[i]->selected]);
-			strcat(formatted, "     ");
+
+			for(int ii = 0; ii < maxinx - 9 - biggest - strlenunicode(options[i]->options[options[i]->selected]); ii++) {
+				strcat(formatted, " ");
+			}
 
 			mvwprintw(inner, 3 + 2 * i, 8 + biggest, formatted);
 
